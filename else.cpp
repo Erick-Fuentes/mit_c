d46bc2ddf4c7dc0affc0117100d6f94bda6c6fbc
Manuel Fuentes <rafaelgodinez275@gmail.com>
2024-02-24 23:53:37 -0600

    primer_commit

---
+++ else.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+main(){
+	
+	
+	
+	char lapiz,lapicero,cuaderno;
+	cout<<"Trae Lapiz (s/n):";
+	cin>>lapiz;
+	cout<<"Trae lapicero (s/n):";
+	cin>>lapicero;
+	cout<<"Trae cuaderno (s/n):";
+	cin>>cuaderno;
+	
+	 if((lapiz=='s' || lapicero=='s') && cuaderno=='s' ){
+	 	cout<<"Ingrese"<<endl;
+	 	
+	 }else{
+	 	cout<<"Lo siento no puede Ingresar"<<endl;
+	 	
+	 }
+	
+	
+	
+}
